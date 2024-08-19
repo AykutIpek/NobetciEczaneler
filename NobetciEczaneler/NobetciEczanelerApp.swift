5de5aa922e026e9acb644ea099d8fab7410feb0f
AykutIpek <aykutipek_2001@hotmail.com>
2024-08-19 13:42:34 +0300

    Initial Commit

---
+++ NobetciEczaneler/NobetciEczanelerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NobetciEczanelerApp.swift
+//  NobetciEczaneler
+//
+//  Created by aykut ipek on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct NobetciEczanelerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
