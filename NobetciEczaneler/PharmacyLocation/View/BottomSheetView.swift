90ca6270aa1a17ed0c743e154a0a9a20da0da368
AykutIpek <aykutipek_2001@hotmail.com>
2024-09-02 13:37:08 +0300

    Bottom sheet view splitted

--- NobetciEczaneler/PharmacyLocation/View/BottomSheetView.swift
+++ NobetciEczaneler/PharmacyLocation/View/BottomSheetView.swift
@@ -15,94 +15,129 @@ struct BottomSheetView: View {
     
     var body: some View {
         VStack(spacing: 16) {
-            HStack(alignment: .firstTextBaseline) {
-                VStack(alignment: .leading, spacing: 6) {
-                    Text(pharmacy.name.orEmptyString)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                    Text(LocalizableString.pharmacy.rawValue.localized)
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                }
-                .padding(.horizontal)
-                Spacer()
-                Button(action: onDismiss) {
-                    Image(systemName: SystemImages.xmarkCircleFill.rawValue)
-                        .font(.title2)
-                        .foregroundColor(.gray)
-                        .padding(.trailing)
-                }
-            }
-            
-            HStack {
-                Button(action: onGetDirections) {
-                    VStack {
-                        Image(systemName: SystemImages.figureWalk.rawValue)
-                            .font(.title2)
-                        Text(LocalizableString.getDirection.rawValue.localized)
-                            .font(.footnote)
-                    }
-                    .foregroundStyle(.white)
-                    .padding(.vertical, 10)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(12)
-                    
-                }
-                
-                Button(action: {
-                    Consts.callPharmacy(phone: pharmacy.phone)
-                }) {
-                    VStack {
-                        Image(systemName: SystemImages.phoneFill.rawValue)
-                            .font(.title2)
-                        Text(LocalizableString.call.rawValue.localized)
-                            .font(.footnote)
-                    }
-                    .foregroundStyle(.gray)
-                    .padding(.vertical, 10)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(12)
-                }
-            }
-            .buttonStyle(PlainButtonStyle())
-            .padding(.horizontal)
-            
+            header
+            buttonContainer
             Divider()
-            
-            VStack(alignment: .leading, spacing: 8) {
-                HStack {
-                    Text(LocalizableString.details.rawValue.localized)
-                        .font(.headline)
-                    Spacer()
-                }
-                
-                if let phone = pharmacy.phone {
-                    HStack {
-                        Text(LocalizableString.telephone.rawValue.localized)
-                        Spacer()
-                        Text(phone)
-                            .foregroundColor(.blue)
-                    }
-                }
-                
-                if let address = pharmacy.address {
-                    HStack(alignment: .top) {
-                        Text(LocalizableString.address.rawValue.localized)
-                        Spacer()
-                        Text(address)
-                            .multilineTextAlignment(.trailing)
-                    }
-                }
-            }
-            .padding(.horizontal)
-            
+            bottomInfo
             Spacer()
         }
         .padding(.top)
         .padding(.bottom, 20)
         .background(.white)
+    }
+    
+    private var header: some View {
+        HStack(alignment: .firstTextBaseline) {
+            title
+            Spacer()
+            closeButton
+        }
+    }
+    
+    private var title: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text(pharmacy.name.orEmptyString)
+                .font(.title2)
+                .fontWeight(.bold)
+            Text(LocalizableString.pharmacy.rawValue.localized)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+        }
+        .padding(.horizontal)
+    }
+    
+    private var closeButton: some View {
+        Button(action: onDismiss) {
+            Image(systemName: SystemImages.xmarkCircleFill.rawValue)
+                .font(.title2)
+                .foregroundColor(.gray)
+                .padding(.trailing)
+        }
+    }
+    
+    private var buttonContainer: some View {
+        HStack {
+            distanceButtonPrimary
+            callButton
+        }
+        .buttonStyle(PlainButtonStyle())
+        .padding(.horizontal)
+    }
+    
+    private var distanceButtonPrimary: some View {
+        Button(action: onGetDirections) {
+            VStack {
+                Image(systemName: SystemImages.figureWalk.rawValue)
+                    .font(.title2)
+                Text(LocalizableString.getDirection.rawValue.localized)
+                    .font(.footnote)
+            }
+            .foregroundStyle(.white)
+            .padding(.vertical, 10)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .cornerRadius(12)
+            
+        }
+    }
+    
+    private var callButton: some View {
+        Button(action: {
+            Consts.callPharmacy(phone: pharmacy.phone)
+        }) {
+            VStack {
+                Image(systemName: SystemImages.phoneFill.rawValue)
+                    .font(.title2)
+                Text(LocalizableString.call.rawValue.localized)
+                    .font(.footnote)
+            }
+            .foregroundStyle(.gray)
+            .padding(.vertical, 10)
+            .frame(maxWidth: .infinity)
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(12)
+        }
+    }
+    
+    private var bottomInfo: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            detailsTitle
+            phoneText
+            addressText
+        }
+        .padding(.horizontal)
+    }
+    
+    private var detailsTitle: some View {
+        HStack {
+            Text(LocalizableString.details.rawValue.localized)
+                .font(.headline)
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private var phoneText: some View {
+        if let phone = pharmacy.phone {
+            HStack {
+                Text(LocalizableString.telephone.rawValue.localized)
+                Spacer()
+                Text(phone)
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var addressText: some View {
+        if let address = pharmacy.address {
+            HStack(alignment: .top) {
+                Text(LocalizableString.address.rawValue.localized)
+                Spacer()
+                Text(address)
+                    .multilineTextAlignment(.trailing)
+            }
+        }
     }
 }
 
